21d774f050a468d1906031c60a82abc74c949d72
Max Vorontsov <joistick11@gmail.com>
2017-02-27 11:47:15 +0300

    added control for music

--- edison_control.py
+++ edison_control.py
@@ -4,29 +4,32 @@ import pyupm_i2clcd as lcd
 import pyupm_buzzer as upmBuzzer
 import time
 
-
 RED_LED_PIN = 2
 GREEN_LED_PIN = 3
 BLUE_LED_PIN = 4
 TEMP_PIN = 0
 BUZZER_PIN = 5
 
-#led init
+# led init
 ledPins = {"R": mraa.Gpio(RED_LED_PIN), "G": mraa.Gpio(GREEN_LED_PIN), "B": mraa.Gpio(BLUE_LED_PIN)}
 [ledPin.dir(mraa.DIR_OUT) for key, ledPin in ledPins.items()]
 
-#temp init
+# temp init
 temp = grove.GroveTemp(TEMP_PIN)
 
-#lcd init
+# lcd init
 lcdDisplay = lcd.Jhd1313m1(0, 0x3E, 0x62)
 lcdDisplay.clear()
 
-#buzzer init
+# buzzer init
 buzzer = upmBuzzer.Buzzer(5)
 buzzer.stopSound()
-notes = [0,10,10,30,40,70,100,130,180,220,270,300,310,290,220,130,560,210,500,500,500,500]
-global playing
+notes = [0, 10, 10, 30, 40, 70, 100, 130, 180, 220, 270, 300, 310, 290, 220, 130, 560, 210, 500, 500, 500, 500]
+global playing_request
+global stop_request
+playing_request = False
+stop_request = False
+
 
 def change_led_state(color):
     led = ledPins.get(str(color))
@@ -40,7 +43,7 @@ def lcd_write(message):
 
 
 def get_temperature():
-    return str(temp.value()-3)
+    return str(temp.value() - 3)
 
 
 def play_sound():
@@ -49,24 +52,31 @@ def play_sound():
     buzzer.stopSound()
 
 
+def stop_music():
+    global stop_request
+    stop_request = True
+
+
 def music_thread():
-	try:
-	    while(True):
-        	if(playing):
-        		for notes_ind in range (0,22):
-        			print("playing " + str(notes[notes_ind]))
-        			if playing == True:
-        				#print("playing " + str(notes[notes_ind]))
-        				buzzer.playSound(notes[notes_ind], 100000)
-	                	time.sleep(0.05)
-	                else:
-						print("waiting butt") 
-						buzzer.stopSound()
-						i = 0
-						while(playing != True):
-							print (i)
-							i = i + 1
-							time.sleep(0.01)
-	except (KeyboardInterrupt, SystemExit):
-		buzzer.stopSound()
-		exit()
+    global playing_request
+    global stop_request
+    try:
+        current_notes = list(notes)
+        while True:
+            while playing_request and not stop_request:
+                if len(current_notes) == 0:
+                    print("Start from beginning")
+                    current_notes = list(notes)
+
+                note = current_notes.pop(0)
+                buzzer.playSound(note, 100000)
+                time.sleep(0.05)
+                print("playing " + str(note))
+            if stop_request:
+                playing_request = False
+                stop_request = False
+                current_notes = list(notes)
+                buzzer.stopSound()
+    except (KeyboardInterrupt, SystemExit):
+        buzzer.stopSound()
+        exit()
--- main_t.py
+++ main_t.py
@@ -4,7 +4,6 @@ from flask import request
 from decorator import crossdomain
 import threading
 
-
 app = Flask(__name__)
 
 
@@ -20,32 +19,35 @@ def temperature():
     return ec.get_temperature()
 
 
-@app.route('/message',  methods=['POST'])
+@app.route('/message', methods=['POST'])
 @crossdomain(origin='*')
 def message():
     ec.lcd_write(str(request.args.get('text')))
     return "Message displayed"
 
 
-@app.route('/sound',  methods=['POST'])
+@app.route('/sound', methods=['POST'])
 @crossdomain(origin='*')
 def sound():
     ec.play_sound()
     return "Sound played"
 
 
-@app.route('/music',  methods=['POST'])
+@app.route('/music', methods=['POST'])
 @crossdomain(origin='*')
 def music():
-	if str(request.args.get('state')) == "playing":
-		ec.playing = True
-	elif str(request.args.get('state')) == "pause":
-		ec.playing = False
-	return "Succ"
+    if str(request.args.get('state')) == "playing":
+        ec.playing_request = True
+    elif str(request.args.get('state')) == "pause":
+        ec.playing_request = False
+    elif str(request.args.get('state')) == "stop":
+        ec.stop_request = True
+    return "Success"
 
 
 if __name__ == '__main__':
-	ec.playing = False
-	thread = threading.Thread(target=ec.music_thread, args=())
-	thread.start()
-	app.run()
+    ec.playing_request = False
+    ec.stop_request = False
+    thread = threading.Thread(target=ec.music_thread, args=())
+    thread.start()
+    app.run()
